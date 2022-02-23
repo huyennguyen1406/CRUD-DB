2dcba99af8dd77c0e416b758fcd6e134ca72f441
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2022-02-23 13:41:20 +0700

    add 1 table

---
+++ CRUD-Product-OneTable/src/main/java/service/IProductRepository.java
@@ -0,0 +1,15 @@
+package service;
+
+import model.Product;
+
+import java.util.ArrayList;
+
+public interface IProductRepository {
+    ArrayList<Product> findAll();
+
+    Product saveProduct(Product product);
+
+    Product deleteProduct(int id);
+
+    Product findProductById(int id);
+}
